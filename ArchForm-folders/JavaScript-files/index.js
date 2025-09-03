f11d4875a2fb7b82d314ffa37b11973f5220be96
Altin Selishta <AltinSelishta14@gmail.com>
2025-09-03 13:55:07 +0200

    Saving the emergency chat form design and 480px breakpoint added

--- ArchForm-folders/JavaScript-files/index.js
+++ ArchForm-folders/JavaScript-files/index.js
@@ -8,3 +8,21 @@ const menuContent = document.getElementById("menuContent");
 Menubtn.addEventListener("click", function(){
     menuContent.classList.toggle("active");
 })
+
+//Here goes the validation form for the emergency chat
+
+const emergencyChat = document.getElementById("emergencyChat");
+const OpenBtn = document.getElementById("OpenBtn");
+const CloseBtn = document.getElementById("CloseBtn");
+
+//Open emergency chat
+OpenBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    emergencyChat.classList.add("open-form");
+});
+//Close emergency chat
+
+CloseBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    emergencyChat.classList.remove("open-form");
+});
