fd70e87a3b8cc96a6935f6d4b6c3a25782e7265e
Altin Selishta <AltinSelishta14@gmail.com>
2025-09-03 14:19:31 +0200

    Saving the emergency chat's form validation function

--- ArchForm-folders/JavaScript-files/index.js
+++ ArchForm-folders/JavaScript-files/index.js
@@ -9,7 +9,7 @@ Menubtn.addEventListener("click", function(){
     menuContent.classList.toggle("active");
 })
 
-//Here goes the validation form for the emergency chat
+//Here goes the open/close form function for the emergency chat
 
 const emergencyChat = document.getElementById("emergencyChat");
 const OpenBtn = document.getElementById("OpenBtn");
@@ -26,3 +26,41 @@ CloseBtn.addEventListener("click", function(e){
     e.preventDefault();
     emergencyChat.classList.remove("open-form");
 });
+
+// Here goes the validation of the emergency chat form
+
+const emergencyForm = document.getElementById("emergencyForm");
+const Name = document.getElementById("Name");
+const TelNumber = document.getElementById("TelNumber");
+const Email = document.getElementById("Email");
+const MsgInput = document.getElementById("MsgInput");
+
+emergencyForm.addEventListener("submit", function(Event){
+    if(Name.value.trim() === "" || TelNumber.value.trim() === "" || Email.value.trim() === "" || MsgInput.value.trim() === "" ){
+        Event.preventDefault();
+        alert("Please fill the required fields");
+    }
+    //Checks if the Phone Number has the right symbol to be a valid number
+    if(!TelNumber.value.startsWith("+")){
+        Event.preventDefault();
+        TelNumber.style.border = "1px solid red";
+        TelNumber.value = "Use the (+) symbol";
+        TelNumber.style.paddingLeft = "2%";
+
+        setTimeout(() =>{
+            TelNumber.style.border = "1px solid darkgray";
+            TelNumber.value = "";
+            TelNumber.style.paddingLeft = "2%";
+        }, 2000);
+    }
+    //Checks if the Email has the right symbols to be a valid email
+    if(!Email.value.includes("@") || !Email.value.includes("."))  {
+        Event.preventDefault();
+        Email.value = "Use the right symbols (@, .)";
+        Email.style.paddingLeft = "2%";
+        setTimeout(() =>{
+            Email.value = "";
+            Email.style.paddingLeft = "2%";
+        }, 2000)
+    }
+})
