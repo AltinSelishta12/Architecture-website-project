38c8bb49fd8ce18614a135c3122c43e7e06c9cb8
Altin Selishta <AltinSelishta14@gmail.com>
2025-11-07 23:10:33 +0100

    AI chatbot integrated and main-buttons added

--- ArchForm-folders/JS-files/index.js
+++ ArchForm-folders/JS-files/index.js
@@ -12,60 +12,16 @@ nav.addEventListener("mouseout", function(){
     nav.classList.remove("addShadow");
 })
 
-const MainMenu = document.querySelector("#MainMenu")
-const OpenMenu = document.querySelector("#OpenMenu")
-const CloseMenu = document.querySelector("#CloseMenu")
 
-OpenMenu.addEventListener("click", function(e){
-    e.preventDefault();
-    MainMenu.classList.add("menu");
-})
+// Code for displaying the main buttons depending on the viewport height
 
-CloseMenu.addEventListener("click", function(){
-    MainMenu.classList.remove("menu");
-})
+ const mainButtons = document.querySelector("#mainButtons");
 
-
-//Send Email input validation goes here
- const sendEmailForm = document.querySelector("#sendEmailForm");
- const email = document.querySelector("#email");
- const emailBtn = document.querySelector("#emailBtn");
-
- sendEmailForm.addEventListener("submit", function(e){
-    e.preventDefault();
-    if(email.value.trim() === ""){
-        alert("Please fill the empty input");
-        return;
-    }
-    else if(!email.value.includes("@") || !email.value.includes(".")) {  //else if(!email.value.includes("@")) this controlls if there is not used @ and stops the form
-        alert("Please use valid symbols");
-        email.value = "";
-        return;
-    }
-    alert("We will contact you as soon as we can.");
-        email.value = "";
- })
-
- //Support textrea validation goes here
- const SupportForm = document.querySelector("#SupportForm");
- const textarea = document.querySelector("#SupportInput")
- const SendMessageBtn = document.querySelector("#sendMessageBtn");
-
- SupportForm.addEventListener("submit", function(stop){
-    stop.preventDefault();
-
-    if(textarea.value.trim() === ""){
-        alert("Please fill the empty input");
-        return;
-    }
-    else if(textarea.value.length < 12){
-        alert("This input must contain at least 12 charachters");
-        textarea.value = "";
+ window.addEventListener("scroll", function(){
+    if(this.window.scrollY > 100){
+        mainButtons.classList.add("active")
     }
     else{
-        alert("Thank you for cooperating with us we will reach you soon");
-        textarea.value = "";
+        mainButtons.classList.remove("active")
     }
  })
-
- 
